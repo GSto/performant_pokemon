b4501b4b5da87b2dfb6bc83151ba4c50db118482
Glenn Stovall <glennstovall@gmail.com>
2020-11-16 11:37:33 -0500

    Implements pokemon data.

--- src/App.js
+++ src/App.js
@@ -1,74 +1,114 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import SortableTableHeader from './sortable_table_header'
 import { useTableSorting, tableSort } from './sortable_tables'
 
 import './App.css'
 import './tailwind.output.css'
 
-const testData = [
-  {
-    fname: 'glenn',
-    lname: 'stovall',
-    score: 250,
-  },
-  {
-    fname: 'richard',
-    lname: 'stovall',
-    score: 200,
-  },
-  {
-    fname: 'kim',
-    lname: 'stovall',
-    score: 500,
-  },
-  {
-    fname: 'seth',
-    lname: 'hines',
-    score: 250,
-  },
-  {
-    fname: 'andrew',
-    lname: 'long',
-    score: -1,
-  },
-]
-
 function App() {
   const [sortKey, sortOrder, updateSorting] = useTableSorting('fname')
-  const [tableData] = useState(testData)
+  const [tableData, setTableData] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json')
+      .then((response) => {
+        setTableData(response.data.pokemon)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
 
   return (
-    <div className="App w-64 mx-auto">
-      <table className="w-full">
-        <thead>
+    <div className="App w-full">
+      <table className="w-3/5 table-auto mx-auto">
+        <thead className="bg-gray-700 text-white w-full">
+          <th className="px-4 py-2" /> {/* image */}
           <SortableTableHeader
-            onClick={() => updateSorting('fname')}
-            selected={sortKey === 'fname'}
+            onClick={() => updateSorting('num')}
+            selected={sortKey === 'num'}
             sortOrder={sortOrder}
+            className="px-4 py-2"
           >
-            First Name
+            Number
           </SortableTableHeader>
           <SortableTableHeader
-            onClick={() => updateSorting('lname')}
-            selected={sortKey === 'lname'}
+            onClick={() => updateSorting('name')}
+            selected={sortKey === 'name'}
             sortOrder={sortOrder}
+            className="px-4 py-2"
           >
-            Last Name
+            Name
           </SortableTableHeader>
           <SortableTableHeader
-            onClick={() => updateSorting('score')}
-            selected={sortKey === 'score'}
+            onClick={() => updateSorting('type')}
+            selected={sortKey === 'type'}
             sortOrder={sortOrder}
+            className="px-4 py-2"
           >
-            Score
+            Type(s)
           </SortableTableHeader>
+          <SortableTableHeader
+            onClick={() => updateSorting('height')}
+            selected={sortKey === 'height'}
+            sortOrder={sortOrder}
+            className="px-4 py-2"
+          >
+            Height
+          </SortableTableHeader>
+          <SortableTableHeader
+            onClick={() => updateSorting('weight')}
+            selected={sortKey === 'weight'}
+            sortOrder={sortOrder}
+            className="px-4 py-2"
+          >
+            Weight
+          </SortableTableHeader>
+          <SortableTableHeader
+            onClick={() => updateSorting('weakness')}
+            selected={sortKey === 'weakness'}
+            sortOrder={sortOrder}
+            className="px-4 py-2"
+          >
+            Weakness(es)
+          </SortableTableHeader>
+
         </thead>
         <tbody>
-          { tableSort(tableData, sortKey, sortOrder).map(({ fname, lname, score }) => (
-            <tr>
-              <td>{ fname }</td>
-              <td>{ lname }</td>
-              <td>{ score }</td>
+          { tableSort(tableData, sortKey, sortOrder).map(({
+            img,
+            num,
+            name,
+            type,
+            height,
+            weight,
+            weaknesses,
+          }) => (
+            <tr key={num} className="border">
+              <td className="py-2">
+                <img src={img} alt={name} className="object-scale-down"/>
+              </td>
+              <td>
+                {num}
+              </td>
+              <td>
+                {name}
+              </td>
+              <td>
+                {type.join(',')}
+              </td>
+              <td>
+                {height}
+              </td>
+              <td>
+                {weight}
+              </td>
+              <td>
+                { weaknesses.join(',')}
+              </td>
             </tr>
           ))}
         </tbody>
