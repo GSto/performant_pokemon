34baa69c739e20c5c1f6f100a303febbd5b6b2cf
Glenn Stovall <glennstovall@gmail.com>
2020-12-03 11:19:26 -0500

    refactors table to its own component.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import SortableTableHeader from './sortable_table_header'
-import { useTableSorting, tableSort } from './sortable_tables'
-import Repeat from './repeat'
-import ViewportOnly from './viewport_only'
+
+import Pokedex from './Pokedex'
 
 import './App.css'
 import './tailwind.output.css'
 
-const SkeletonRow = () => (
-  <tr className="border min-h-4">
-    <Repeat times={7}><td className="py-2">&nbsp;</td></Repeat>
-  </tr>
-)
+
+export const API_URL = 'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json'
 
 function App() {
-  const [sortKey, sortOrder, updateSorting] = useTableSorting('fname')
-  const [tableData, setTableData] = useState([])
+  const [pokemon, setPokemon] = useState([])
 
   useEffect(() => {
     axios
-      .get('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json')
+      .get(API_URL)
       .then((response) => {
-        setTableData(response.data.pokemon)
+        setPokemon(response.data.pokemon)
       })
       .catch((error) => {
         console.log(error)
@@ -32,97 +26,8 @@ function App() {
 
   return (
     <div className="App w-full">
-      <table className="w-4/5 mt-20 table-fixed mx-auto">
-        <thead className="bg-gray-700 text-white">
-          <th className="px-4 py-2" /> {/* image */}
-          <SortableTableHeader
-            onClick={() => updateSorting('num')}
-            selected={sortKey === 'num'}
-            sortOrder={sortOrder}
-            className="px-4 py-2"
-          >
-            Number
-          </SortableTableHeader>
-          <SortableTableHeader
-            onClick={() => updateSorting('name')}
-            selected={sortKey === 'name'}
-            sortOrder={sortOrder}
-            className="px-4 py-2"
-          >
-            Name
-          </SortableTableHeader>
-          <SortableTableHeader
-            onClick={() => updateSorting('type')}
-            selected={sortKey === 'type'}
-            sortOrder={sortOrder}
-            className="px-4 py-2"
-          >
-            Type(s)
-          </SortableTableHeader>
-          <SortableTableHeader
-            onClick={() => updateSorting('height')}
-            selected={sortKey === 'height'}
-            sortOrder={sortOrder}
-            className="px-4 py-2"
-          >
-            Height
-          </SortableTableHeader>
-          <SortableTableHeader
-            onClick={() => updateSorting('weight')}
-            selected={sortKey === 'weight'}
-            sortOrder={sortOrder}
-            className="px-4 py-2"
-          >
-            Weight
-          </SortableTableHeader>
-          <SortableTableHeader
-            onClick={() => updateSorting('weakness')}
-            selected={sortKey === 'weakness'}
-            sortOrder={sortOrder}
-            className="px-4 py-2"
-          >
-            Weakness(es)
-          </SortableTableHeader>
-
-        </thead>
-        <tbody>
-          { tableSort(tableData, sortKey, sortOrder).map(({
-            img,
-            num,
-            name,
-            type,
-            height,
-            weight,
-            weaknesses,
-          }) => (
-            <ViewportOnly placeholder={<SkeletonRow />}>
-              <tr key={num} className="border">
-                <td className="py-2">
-                  <img src={img} alt={name} className="object-scale-down"/>
-                </td>
-                <td>
-                  {num}
-                </td>
-                <td>
-                  {name}
-                </td>
-                <td>
-                  {type.join(', ')}
-                </td>
-                <td>
-                  {height}
-                </td>
-                <td>
-                  {weight}
-                </td>
-                <td>
-                  { weaknesses.join(', ')}
-                </td>
-              </tr>
-          </ViewportOnly>
-          ))}
-        </tbody>
-      </table>
+      <h1>Pokemeon sortable table demo</h1>
+      <Pokedex pokemon={pokemon} />
     </div>
   );
 }
---
+++ src/Pokedex.js
@@ -0,0 +1,110 @@
+import SortableTableHeader from './sortable_table_header'
+import { useTableSorting, tableSort } from './sortable_tables'
+import ViewportOnly from './viewport_only'
+import Repeat from './repeat'
+
+export function SkeletonRow() {
+  return (
+    <tr className="border min-h-4">
+      <Repeat times={7}><td className="py-2">&nbsp;</td></Repeat>
+    </tr>
+  )
+}
+
+export default function Pokedex({ pokemon }) {
+  const [sortKey, sortOrder, updateSorting] = useTableSorting('fname')
+  return (
+    <table className="w-4/5 mt-20 table-fixed mx-auto">
+      <thead className="bg-gray-700 text-white">
+        <tr>
+          <th className="px-4 py-2" />{/* image */}
+          <SortableTableHeader
+            onClick={() => updateSorting('num')}
+            selected={sortKey === 'num'}
+            sortOrder={sortOrder}
+            className="px-4 py-2"
+          >
+            Number
+          </SortableTableHeader>
+          <SortableTableHeader
+            onClick={() => updateSorting('name')}
+            selected={sortKey === 'name'}
+            sortOrder={sortOrder}
+            className="px-4 py-2"
+          >
+            Name
+          </SortableTableHeader>
+          <SortableTableHeader
+            onClick={() => updateSorting('type')}
+            selected={sortKey === 'type'}
+            sortOrder={sortOrder}
+            className="px-4 py-2"
+          >
+            Type(s)
+          </SortableTableHeader>
+          <SortableTableHeader
+            onClick={() => updateSorting('height')}
+            selected={sortKey === 'height'}
+            sortOrder={sortOrder}
+            className="px-4 py-2"
+          >
+            Height
+          </SortableTableHeader>
+          <SortableTableHeader
+            onClick={() => updateSorting('weight')}
+            selected={sortKey === 'weight'}
+            sortOrder={sortOrder}
+            className="px-4 py-2"
+          >
+            Weight
+          </SortableTableHeader>
+          <SortableTableHeader
+            onClick={() => updateSorting('weakness')}
+            selected={sortKey === 'weakness'}
+            sortOrder={sortOrder}
+            className="px-4 py-2"
+          >
+            Weakness(es)
+          </SortableTableHeader>
+        </tr>
+      </thead>
+    <tbody>
+      { tableSort(pokemon, sortKey, sortOrder).map(({
+        img,
+        num,
+        name,
+        type,
+        height,
+        weight,
+        weaknesses,
+      }) => (
+        <ViewportOnly placeholder={<SkeletonRow />}>
+          <tr key={num} className="border">
+            <td className="py-2">
+              <img src={img} alt={name} className="object-scale-down"/>
+            </td>
+            <td>
+              {num}
+            </td>
+            <td>
+              {name}
+            </td>
+            <td>
+              {type.join(', ')}
+            </td>
+            <td>
+              {height}
+            </td>
+            <td>
+              {weight}
+            </td>
+            <td>
+              { weaknesses.join(', ')}
+            </td>
+          </tr>
+      </ViewportOnly>
+      ))}
+    </tbody>
+  </table>
+  )
+}
